bf8d23da8c2d4279db6731c2d089592e5dac442e
Prasenjit Gupta <gupta.prasenjit@gmail.com>
2017-02-09 00:13:20 +0530

    flask app updates

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -1,8 +1,12 @@
 # ---- Flask Hello World ---- #
 # import the Flask class from the flask package
 from flask import Flask
+
+
 # create the application object
 app = Flask(__name__)
+
+
 # use the decorator pattern to
 # link the view function to a url
 @app.route("/")
@@ -10,6 +14,34 @@ app = Flask(__name__)
 # define the view using a function, which returns a string
 def hello_world():
     return "Hello, World!"
+    
+@app.route("/integer/<int:value>")
+def	int_type(value):
+	print(value + 1)
+	return "correct"
+	
+@app.route("/float/<float:value>")
+def	float_type(value):
+	print(value + 1)
+	return "correct"
+	
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def	path_type(value):
+	print(value)
+	return "correct"    
+
+@app.route("/test/<search_query>")
+def	search(search_query):
+	return	search_query
+	
+@app.route("/name/<name>")
+def	index(name):
+	if name.lower()	== "michael":
+		return "Hello, {}".format(name),200
+	else:
+		return "Not Found", 404
+
 # start the development server using the run() method
 if __name__ == "__main__":
     app.run()
